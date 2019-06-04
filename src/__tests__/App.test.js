4b9dd9d8c27ce013e370178456e527e4fccffe39
conaryh <conaryh@gmail.com>
2019-06-04 11:01:25 -0600

    modifed package json added enzyme dependency and first test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,1 +1,10 @@
-// code here
+import React from 'react';
+import App from '../App';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+it('should match snapshot', () => {
+	const tree = renderer.create(<App />).toJSON();
+
+	expect(tree).toMatchSnapshot();
+});
