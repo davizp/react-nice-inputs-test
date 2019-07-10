23715728551423efc351415399d73c271db7a67e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-10 20:09:01 +0000

    Deploy davizp/react-nice-inputs-test to github.com/davizp/react-nice-inputs-test.git:gh-pages

---
+++ precache-manifest.e7aa4d063afb9e3ae1cfa58299ce6d4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a7615a49a643fb8ff03dcbb828df5d",
+    "url": "/react-nice-inputs-test/index.html"
+  },
+  {
+    "revision": "0737993ab17a4d8e92a9",
+    "url": "/react-nice-inputs-test/static/css/main.e724b666.chunk.css"
+  },
+  {
+    "revision": "43c12e3ee012035b78ec",
+    "url": "/react-nice-inputs-test/static/js/2.31fee128.chunk.js"
+  },
+  {
+    "revision": "0737993ab17a4d8e92a9",
+    "url": "/react-nice-inputs-test/static/js/main.84a69669.chunk.js"
+  },
+  {
+    "revision": "942c96dc808ad2e23660",
+    "url": "/react-nice-inputs-test/static/js/runtime~main.eddf6b7f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-nice-inputs-test/precache-manifest.e7aa4d063afb9e3ae1cfa58299ce6d4b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-nice-inputs-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
